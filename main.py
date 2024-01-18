daa1461515474cecefff5a00aa822229ffc48822
Ayman-Hachem <ayman.hachem@laplateforme.io>
2024-01-18 11:18:20 +0100

    update

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 
 from game import Game
@@ -8,18 +7,3 @@ pygame.init()
 if __name__ == "__main__":
     game = Game()
     game.run()
-
-
-=======
-import pygame
-
-from game import Game
-
-pygame.init()
-
-if __name__ == "__main__":
-    game = Game()
-    game.run()
-
-
->>>>>>> 0adbc4d4c15ec05d86043d72acc2396a33c0c20b
--- screen.py
+++ screen.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 
 
@@ -19,26 +18,5 @@ class Screen:
         return self.display.get_size()
 
     def get_display(self):
-=======
-import pygame
 
-
-class Screen:
-    def __init__(self):
-        self.display = pygame.display.set_mode((1200, 720))
-        pygame.display.set_caption("Pokémon")
-        self.clock = pygame.time.Clock()
-        self.framerate = 60
-
-    def update(self):
-        pygame.display.flip()
-        pygame.display.update()
-        self.clock.tick(self.framerate)
-        self.display.fill((0, 0, 0))
-
-    def get_size(self):
-        return self.display.get_size()
-
-    def get_display(self):
->>>>>>> 0adbc4d4c15ec05d86043d72acc2396a33c0c20b
         return self.display
